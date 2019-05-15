dcb1d8473b5399f300616d4210ed4cbdd69ca67d
Daniel Camarena <dcamarena0229@gmail.com>
2019-05-14 20:49:01 -0700

    First commit. Completing phase one.

---
+++ models/skill.js
@@ -0,0 +1,25 @@
+const skills = [
+    {skill: 'HTML', learned: true},
+    {skill: 'CSS', learned: true},
+    {skill: 'JS', learned: true},
+    {skill: 'jQuery', learned: true},
+    {skill: 'Python', learned: false},
+    {skill: 'Ruby', learned: false},
+    {skill: 'React', learned: false},
+    {skill: 'Express', learned: true},
+    {skill: 'EJS', learned: true},
+]
+
+module.exports = {
+ getAll,
+ getOne
+};
+
+
+function getOne(id) {
+    return skills[id];
+}
+
+function getAll() {
+    return skills;
+}
